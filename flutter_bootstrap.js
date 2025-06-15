7f6fad6fbbe841e58b31b11a3423fcac76b62dbb
suyash-jathar <suyashjathar15@gmail.com>
2025-06-15 20:12:38 +0530

    Updating quantity feature

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2969799561"
+    serviceWorkerVersion: "1140463646"
   }
 });
